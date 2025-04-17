4c609ee11db9c82f0c65b23f9d601ecd50952d50
Felippe Santana <f-elipp-3@hotmail.com>
2025-04-17 13:01:17 +0100

    ft_strlcpy.c reworked

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -1,16 +1,6 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: fde-alme <fde-alme@student.42porto.com>    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/04/08 14:38:38 by fde-alme          #+#    #+#             */
-/*   Updated: 2025/04/09 20:50:53 by fde-alme         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
+#include <stdio.h>
+#include <string.h>
 
 size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 {
@@ -18,13 +8,40 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	size_t	i;
 
 	src_len = ft_strlen(src);
-	i = 0;
-	while (i < (size - 1))
+	if (src_len > 0)
 	{
-		dst[i] = src[i];
-		i++;
-	}
-	if (size > 0)
+		i = 0;
+		while (i < size - 1 && i < src_len)
+		{
+			dst[i] = src[i];
+			i++;
+		}
 		dst[i] = '\0';
+	}
 	return (src_len);
+}
+
+int	main(void)
+{
+	char	buffer[17];
+	char	buffer2[10];
+	char	buffer3[10];
+	char	*src;
+
+	src = "Copy this string";
+	printf("My function: %lu\n", ft_strlcpy(buffer, src, sizeof(buffer)));
+	printf("Original function: %lu\n", strlcpy(buffer, src, sizeof(buffer)));
+	
+	printf("My function: %lu\n", ft_strlcpy(buffer, src, sizeof(buffer2)));
+	printf("Original function: %lu\n", strlcpy(buffer, src, sizeof(buffer2)));
+
+	src = "";
+	printf("My function: %lu\n", ft_strlcpy(buffer3, src, sizeof(buffer2)));
+	printf("Original function: %lu\n", strlcpy(buffer3, src, sizeof(buffer2)));
+
+	src = "Test";
+	printf("My function: %lu\n", ft_strlcpy(buffer3, src, sizeof(buffer3)));
+	printf("Original function: %lu\n", strlcpy(buffer3, src, sizeof(buffer3)));
+
+	return (0);
 }

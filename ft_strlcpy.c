5d65fae5d8905a858c2c019d0597842983f855df
Felippe Santana <f-elipp-3@hotmail.com>
2025-04-17 13:28:40 +0100

    strlcpy.c rework 2

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -7,17 +7,14 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size)
 	size_t	src_len;
 	size_t	i;
 
+	i = 0;
 	src_len = ft_strlen(src);
-	if (src_len > 0)
+	while (i < size - 1 && i < src_len)
 	{
-		i = 0;
-		while (i < size - 1 && i < src_len)
-		{
-			dst[i] = src[i];
-			i++;
-		}
-		dst[i] = '\0';
+		dst[i] = src[i];
+		i++;
 	}
+	dst[i] = '\0';
 	return (src_len);
 }
 
@@ -27,21 +24,18 @@ int	main(void)
 	char	buffer2[10];
 	char	buffer3[10];
 	char	*src;
+	char	*buff;
+	size_t	i;
 
-	src = "Copy this string";
-	printf("My function: %lu\n", ft_strlcpy(buffer, src, sizeof(buffer)));
-	printf("Original function: %lu\n", strlcpy(buffer, src, sizeof(buffer)));
-	
-	printf("My function: %lu\n", ft_strlcpy(buffer, src, sizeof(buffer2)));
 	printf("Original function: %lu\n", strlcpy(buffer, src, sizeof(buffer2)));
 
-	src = "";
-	printf("My function: %lu\n", ft_strlcpy(buffer3, src, sizeof(buffer2)));
-	printf("Original function: %lu\n", strlcpy(buffer3, src, sizeof(buffer2)));
-
-	src = "Test";
-	printf("My function: %lu\n", ft_strlcpy(buffer3, src, sizeof(buffer3)));
-	printf("Original function: %lu\n", strlcpy(buffer3, src, sizeof(buffer3)));
+//	src = "";
+//	printf("My function: %lu\n", ft_strlcpy(buffer3, src, sizeof(buffer2)));
+//	printf("Original function: %lu\n", strlcpy(buffer3, src, sizeof(buffer2)));
+//
+//	src = "Test";
+//	printf("My function: %lu\n", ft_strlcpy(buffer3, src, sizeof(buffer3)));
+//	printf("Original function: %lu\n", strlcpy(buffer3, src, sizeof(buffer3)));
 
 	return (0);
 }
